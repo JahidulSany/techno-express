12e72e6e5ce53da065160b5c29be1c2cbb2e12a3
JahidulSany <jahidulsanypro@gmail.com>
2026-03-03 15:50:14 +0000

    Initialised Express Server and Sequelize Database Setup

---
+++ server.js
@@ -0,0 +1,34 @@
+// Import required packages
+const express = require('express');
+const path = require('path');
+
+const sequelize = require('./config/connection');
+// const routes = require('./routes');
+
+// Initialize Express application
+const app = express();
+
+// Middlewares to parse incoming JSON requests
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const PORT = process.env.PORT || 3001;
+
+// has the --rebuild parameter been passed as a command line param?
+const rebuild = process.argv[2] === '--rebuild';
+
+// Middleware to serve static files from the "Public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Handle GET request at the root route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Add routes
+// app.use(routes);
+
+// Sync database
+sequelize.sync({ force: rebuild }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening at port: ${PORT}`));
+});
