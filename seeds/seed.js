9950ffaba4c08c0e2611b85b043c19419953185f
JahidulSany <jahidulsanypro@gmail.com>
2026-03-05 01:21:14 +0000

    Add routes and models for categories and posts; update seed data

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,20 +1,20 @@
 // Import required packages
-const sequelize = require("../config/connection");
+const sequelize = require('../config/connection');
 
 // import models
-const { Post } = require("../models");
+const { User, Category, Post } = require('../models');
 
 // import seed data
-const postData = require("./blogs.json");
-
-// add data and seeding for Category model
-
+const blogsData = require('./blogs.json');
+const { users, posts, categories } = blogsData;
 
 // Seed database
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  await Post.bulkCreate(postData);
+  await User.bulkCreate(users);
+  await Category.bulkCreate(categories);
+  await Post.bulkCreate(posts);
 
   process.exit(0);
 };
