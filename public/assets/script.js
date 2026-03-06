304f4cc1654f6961ac99011b312d637b1511172c
JahidulSany <jahidulsanypro@gmail.com>
2026-03-06 22:33:37 +0000

    Add token management in login function and ensure proper user authentication flow

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,3 +1,5 @@
+let token = localStorage.getItem("authToken");
+
 function register() {
   const username = document.getElementById('username').value;
   const email = document.getElementById('email').value;
@@ -23,4 +25,37 @@ function register() {
   document.getElementById('username').value = '';
   document.getElementById('email').value = '';
   document.getElementById('password').value = '';
+}
+
+
+function login() {
+  const email = document.getElementById("login-email").value;
+  const password = document.getElementById("login-password").value;
+  fetch("http://localhost:3001/api/users/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      // Save the token in the local storage
+      if (data.token) {
+        localStorage.setItem("authToken", data.token);
+        token = data.token;
+
+        alert("User Logged In successfully");
+
+        // Fetch the posts list
+        fetchPosts();
+
+        // Hide the auth container and show the app container as we're now logged in
+        document.getElementById("auth-container").classList.add("hidden");
+        document.getElementById("app-container").classList.remove("hidden");
+      } else {
+        alert(data.message);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
